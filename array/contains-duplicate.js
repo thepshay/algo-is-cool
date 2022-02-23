330f13b66777ff084163f4000da025932b0fa737
thepshay <perry.xie1@gmail.com>
2022-02-22 16:17:38 -0800

    Contains Duplicate

---
+++ array/contains-duplicate.js
@@ -0,0 +1,25 @@
+// Naive Solution
+// For each element loop through the array again, and check if there is duplicate value
+// If yes return true, else continue
+// If we reach the end without returning true, return false
+// o(n^2) time complexity, n length of array
+// o(1) space complexity
+
+// o(n) solution
+// Start with empty hash 
+// Iterate through the array, check if ele is a key, if yes return true, if not continue 
+// Return false at end of array
+// o(n) time complexity
+// o(n) space complexity
+
+var containsDuplicate = function(nums) {
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+      if (hash[nums[i]]) {
+          return true;
+      } else {
+          hash[nums[i]] = true;
+      }
+  }
+  return false;
+};
